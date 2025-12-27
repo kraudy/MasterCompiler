afd966136a13a909fd39b84f9535eaada52e34f2
Roberto Torrez <robkraudy@gmail.com>
2025-12-27 14:50:21 -0600

    Init commit
    Signed-off-by: Roberto Torrez <robkraudy@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,157 @@
+
+<div style="text-align: center;">
+  <img src="./images/Master%20Compiler.jpg" alt="alt text" style="width: 20%; max-width: 800px; height: auto;">
+</div>
+
 # MasterCompiler
-Master compilation tool for the IBM i platform
+
+Master compilation tool for the IBM i platform. 
+
+YAML-based, sensible, open-source, modern, clean, and elegant approach to automate and streamline object compilation.
+
+Do you like building cool stuff for fun and freedom? Then this project is for you.
+
+---
+
+This project aims to give the whole IBM i community a standard way to describe the compilation flow of objects in a simple, elegant, modern, and clean manner. Affords easy integration with any DevOps pipeline; could be run locally (just upload the JAR file) or remotely in a Docker container.
+
+## Requirementes
+
+Java 8
+
+---
+
+## Compilation target?
+
+To compile an object, it must be defined as a unique key of the form **library.name.objectType.sourceType**. The **objectType** and **sourceType** of the target key define the compilation command to be executed.
+
+* mylib.hello.pgm.rpgle         => CRTBNDRPG
+* mylib.sqlhello.pgm.sqlrpgle   => CRTSQLRPGI
+* mylib.dsphello.dspf.dds       => CRTDSPF
+* mylib.tabhello.table.sql      => RUNSQLSTM
+* mylib.modhello.module.rpgle   => CRTRPGMOD
+* mylib.srvhello.srvpgm.bnd     => CRTSRVPGM
+
+[Object types](./src/main/java/com/github/kraudy/compiler/CompilationPattern.java#L133) 
+
+[Sourece types](./src/main/java/com/github/kraudy/compiler/CompilationPattern.java#L69) 
+
+[Compilation command from Object type and Sourece types](./src/main/java/com/github/kraudy/compiler/CompilationPattern.java#L239) 
+
+[Param pattern from Compilation command](./src/main/java/com/github/kraudy/compiler/CompilationPattern.java#L1064) 
+
+---
+
+## Flow of compilation?
+
+Define a yaml file
+
+[Full spec here](./src/main/java/com/github/kraudy/compiler/BuildSpec.java) 
+[Hooks deserializer](./src/main/java/com/github/kraudy/compiler/CommandMapDeserializer.java) 
+[Compilation params deserializer](./src/main/java/com/github/kraudy/compiler/ParamMapDeserializer.java) 
+[More yaml examples]() 
+
+```yaml
+defaults: {}  # optional | Global compilation command params
+
+before:       # optional | Global pre-compilation system commands
+  chglibl:                # cl command
+    libl: mylib1 mylib2   # cl command param
+
+after: {}     # optional | Global post-compilation system commands
+
+success: {}   # optional | Global on success system commands
+
+failure: {}   # optional | Global on success system commands
+
+targets:      # Required | Global on success system commands
+
+  mylib1.hello.pgm.rgple:  # Required | Compilation target
+
+    params:               # Required | Per-target compilation command params
+      # Compilation params
+      SRCSTMF: /home/sources/HELLO.RPGLE
+      DFTACTGRP: no
+      ACTGRP: QILE
+      STGMDL: Snglvl      # note how params names are flexible
+      OPTION: EVENTF
+      DBGVIEW: source
+      REPLACE: yes
+      USRPRF: user
+      TGTRLS: V7R5M0
+      PRFDTA: nocol
+
+    before: {}            # optional | Per-target pre-compilation system commands
+
+    after: {}             # optional | Per-target post-compilation system commands
+
+    success: {}           # optional | Per-target on success system commands
+
+    failure: {}           # optional | Per-target on success system commands
+
+  # You can define more targets, each one with its own hooks and params
+  mylib2.sqlhello.pgm.sqlrpgle: 
+
+    #params:                no params, will use defaults
+
+    before: 
+      chgcurlib:          # change cur lib before target compilation
+        CURLIB: mylib2
+    
+    # ignore other hooks
+
+  # A third target with no params and no hooks. Simplest case
+  mylib2.modhello.module.rpgle: {} 
+
+```
+
+Then just call MasterCompiler like this: **java -jar MasterCompiler-1.0-SNAPSHOT.jar -xv -f /home/user/clean_spect.yaml**
+
+Pretty cool, right? Well, here is more:
+
+
+## Automatic migration
+
+MC encourages this by automatically migrating source members to ifs stream files and stream files to source members for OPM objects. 
+
+[Migrator](./src/main/java/com/github/kraudy/compiler/Migrator.java) 
+
+
+## Automatic param inspection
+
+MC tries to extract compilation params from existing objects.
+
+[ObjectDescriptor](./src/main/java/com/github/kraudy/compiler/ObjectDescriptor.java) 
+
+
+## Automatic param resolution
+
+Conflicts between command params are automatically resolved.
+
+[Resolve conflict](./src/main/java/com/github/kraudy/compiler/ParamMap.java#L153) 
+
+## Automatic param validation
+
+If an invalid param is provided for a command, an exception is thrown.
+
+[Reject invalid param](./src/main/java/com/github/kraudy/compiler/ParamMap.java#L91) 
+
+## Unix style CLI
+
+* YAML file route:  `-f | --file /route/cool_spec.yaml`
+* Debug and verbose mode flags:` -x, -v | -xv`
+* Dry run to generate command strings without execution: `--dry-run `
+* Diff run to build only what has changed: `--diff`
+
+[Argument parser](./src/main/java/com/github/kraudy/compiler/ParamMap.ArgParser) 
+
+## Traceability
+
+Fully transparent and traceble flow of execution and changes.
+
+* All messages are logs 
+* Exceptions are handled and raised. No context loss. 
+* Each param change is tracked individually
+* Each command executed is tracked in a chain of commands
+
+[Argument parser](./src/main/java/com/github/kraudy/compiler/ParamMap.ArgParser) 
---
+++ src/main/java/com/github/kraudy/compiler/MasterCompiler.java
@@ -0,0 +1,301 @@
+package com.github.kraudy.compiler;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.ibm.as400.access.AS400;
+import com.ibm.as400.access.AS400JDBCDataSource;
+import com.ibm.as400.access.User;
+
+import io.github.theprez.dotenv_ibmi.IBMiDotEnv;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ * Master compiler for the IBM I platform.
+ */
+public class MasterCompiler{
+  private static final Logger logger = LoggerFactory.getLogger(MasterCompiler.class); // Per-class logger
+
+  public static final String INVARIANT_CCSID = "37"; // EBCDIC
+  public static final String UTF8_CCSID = "1208";
+  private final AS400 system;
+  private final Connection connection;
+  private final User currentUser;
+  private CommandExecutor commandExec;
+  private Migrator migrator;
+  private ObjectDescriptor odes;
+  private SourceDescriptor sourceDes;
+
+  private BuildSpec globalSpec;     // global build spec
+  private boolean dryRun = false;   // Compile commands without executing 
+  private boolean debug = false;    // Debug flag
+  private boolean verbose = false;  // Verbose output flag
+  private boolean diff = false;     // Diff build flag
+
+
+  public MasterCompiler(AS400 system) throws Exception {
+    this(system, new AS400JDBCDataSource(system).getConnection());
+  }
+
+  public MasterCompiler(AS400 system, Connection connection) throws Exception {
+    this.system = system;
+
+    // Database
+    this.connection = connection;
+    this.connection.setAutoCommit(true);
+
+    // User
+    this.currentUser = new User(system, system.getUserId());
+    this.currentUser.loadUserInformation();
+
+  }
+
+  public MasterCompiler(AS400 system, BuildSpec globalSpec, boolean dryRun, boolean debug, boolean verbose, boolean diff) throws Exception {
+    this(system, new AS400JDBCDataSource(system).getConnection());
+
+    /* Set params */
+    this.globalSpec = globalSpec;
+    this.dryRun = dryRun;
+    this.debug = debug;
+    this.verbose = verbose;
+    this.diff = diff;
+  }
+
+  public void build() {
+
+    /* Init command executor */
+    commandExec = new CommandExecutor(connection, debug, verbose, dryRun);
+
+    /* Init migrator */
+    migrator = new Migrator(connection, debug, verbose, currentUser, commandExec);
+
+    /* Init source descriptor */
+    sourceDes = new SourceDescriptor(connection, debug, verbose);
+
+    /* Init object descriptor */
+    odes = new ObjectDescriptor(connection, debug, verbose);
+
+    try {
+      /* Global before */
+      if(!globalSpec.before.isEmpty()){
+        if (verbose) logger.error("\nExecuting global before");
+        commandExec.executeCommand(globalSpec.before);
+      }
+
+      if(verbose) logger.info(showLibraryList());
+
+      /* Build each target */
+      buildTargets(globalSpec.targets);
+
+      /* Execute global after */
+      if(!globalSpec.after.isEmpty()){
+        if (verbose) logger.error("\nExecuting global after");
+        commandExec.executeCommand(globalSpec.after);
+      }
+
+      /* Execute global success */
+      if(!globalSpec.success.isEmpty()){
+        if (verbose) logger.error("\nExecuting global success");
+        commandExec.executeCommand(globalSpec.success);
+      }
+      
+    } catch (CompilerException e){
+      if (verbose) logger.error("\nCompilation failed");
+
+      /* Get full compiler exception context */
+      logger.error(e.getFullContext());
+
+      /* Global compiler failure */
+      try{
+        if(!globalSpec.failure.isEmpty()){
+          if (verbose) logger.error("\nExecuting global failure");
+          commandExec.executeCommand(globalSpec.failure);
+        }
+      } catch (Exception failureErr) {
+          throw new CompilerException("Target failure hook also failed", failureErr);
+      }
+
+    } catch (Exception e) {
+      /* Unhandled Exception. Fail loudly */
+      logger.error("\nUnhandled Exception. Fail loudly", e);
+
+    } finally {
+      /* Show chain of commands */
+      if (verbose) logger.info("\nChain of commands: {}", commandExec.getExecutionChain());
+
+      cleanup();
+    }
+
+  }
+
+  private void buildTargets(LinkedHashMap<TargetKey, BuildSpec.TargetSpec> targets) throws Exception{
+    /* This is intended for a YAML file with multiple objects in a toposort order */
+    for (Map.Entry<TargetKey, BuildSpec.TargetSpec> entry : targets.entrySet()) {
+      TargetKey key = entry.getKey();
+      BuildSpec.TargetSpec targetSpec = entry.getValue();
+
+      /* Skip target if diff and no build required */
+      if (diff) {
+        sourceDes.getObjectTimestamps(key);
+        if (!key.needsRebuild()) {
+          if (verbose) logger.info("\nSkipping unchanged target: " + key.asString() + key.getTimestmaps());
+          continue; 
+        }
+      }
+
+      if (verbose) logger.info("\nBuilding: " + key.asString());
+
+      try{
+
+        /* Resolve curlib */
+        if(key.isCurLib()){
+          if (verbose) logger.error("\nResolving curlib");
+          key.setLibrary(getCurLIb());
+        }
+
+        /* Per target before */
+        if(!targetSpec.before.isEmpty()){
+          if (verbose) logger.error("\nExecuting target before");
+          commandExec.executeCommand(targetSpec.before);
+        }
+
+        /* If the object exists, we try to extract its compilation params */
+        odes.getObjectInfo(key);
+
+        /* Set global defaults params per target */
+        key.putAll(globalSpec.defaults);
+
+        /* Set specific target params */
+        key.putAll(targetSpec.params);
+
+        /* Migrate source file */
+        migrator.migrateSource(key);
+
+        /* Execute compilation command */
+        commandExec.executeCommand(key);
+
+        /* Per target after */
+        if(!targetSpec.after.isEmpty()){
+          if (verbose) logger.error("\nExecuting target after");
+          commandExec.executeCommand(targetSpec.after);
+        } 
+
+        /* Per target success */
+        if(!targetSpec.success.isEmpty()){
+          if (verbose) logger.error("\nExecuting target success");
+          commandExec.executeCommand(targetSpec.success);
+        } 
+
+      } catch (CompilerException e){
+        if (verbose) logger.error("Target compilation failed: " + key.asString());
+
+        /* Per target failure */
+        if(!targetSpec.failure.isEmpty()){
+          if (verbose) logger.error("\nExecuting target failure");
+          commandExec.executeCommand(targetSpec.failure);
+        } 
+
+        throw e; // Raise
+
+      } catch (Exception e){
+        if (verbose) logger.error("Unhandled exception in Target: " + key.asString());
+
+        throw e; // Raise
+
+      } finally {
+        //TODO: Do something cool here.
+      }
+    }
+
+  }  
+
+  private String showLibraryList() throws SQLException{
+    StringBuilder sb = new StringBuilder();;
+    sb.append("\nLibrary list: \n");
+    try(Statement stmt = connection.createStatement();
+        ResultSet rsLibList = stmt.executeQuery(
+          "SELECT DISTINCT(SCHEMA_NAME) As Libraries FROM QSYS2.LIBRARY_LIST_INFO " + 
+          "WHERE TYPE NOT IN ('SYSTEM','PRODUCT') AND SCHEMA_NAME NOT IN ('QGPL', 'GAMES400')"
+          
+        )){
+      while (rsLibList.next()) {
+        sb.append(rsLibList.getString("Libraries")).append("\n");
+      }
+      return sb.toString();
+
+    } catch (SQLException e){
+      logger.error("Error retrieving library list");
+      throw e;
+    }
+  }
+
+  private String getCurLIb() throws SQLException{
+    String curlib = "";
+
+    try(Statement stmt = connection.createStatement();
+        ResultSet rsCurLib = stmt.executeQuery(
+          "SELECT TRIM(SCHEMA_NAME) As SCHEMA_NAME FROM QSYS2.LIBRARY_LIST_INFO WHERE TYPE = 'CURRENT'" 
+        )){
+      if (!rsCurLib.next()) {
+        throw new CompilerException("Error retrieving current library");
+      }
+      curlib = rsCurLib.getString("SCHEMA_NAME");
+      return curlib;
+
+
+    } catch (SQLException e){
+      throw new CompilerException("Error retrieving current library", e);
+    }
+  }
+
+  private void cleanup(){
+    try {
+      if (connection != null && !connection.isClosed()) {
+        connection.close();
+      }
+      if (system != null) {
+        system.disconnectAllServices();
+      }
+
+    } catch (SQLException e) {
+      logger.error("Error cleaning up", e);
+    }
+  }
+
+  public static void main(String... args ){
+    AS400 system = null;
+    MasterCompiler compiler = null;
+    try {
+      ArgParser parser = new ArgParser(args);
+      //TODO: This should be able to run locally in debug mode.
+      if (args.length == 0) throw new IllegalArgumentException("Params are required");
+        
+      system = IBMiDotEnv.getNewSystemConnection(true); // Get system
+      compiler = new MasterCompiler(
+            system,
+            parser.getSpecFromYamlFile(),
+            parser.isDryRun(),
+            parser.isDebug(),
+            parser.isVerbose(),
+            parser.isDiff()
+        );
+      compiler.build();
+
+    } catch (IllegalArgumentException e) {
+      logger.error("Parsing error: ", e);
+      logger.info(ArgParser.getUsage());
+      
+    } catch (CompilerException e){
+      logger.error(e.getFullContext());
+
+    }catch (Exception e) {
+      logger.error("Unhandled  exception", e);
+    }
+  }
+}
